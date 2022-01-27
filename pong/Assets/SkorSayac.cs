40b3bbc21f8ec2564f4930a327a83373de34ffd8
mastikacation <mastikacation@outlook.com>
2022-01-27 22:10:54 +0300

    Build and Project Files
    All of the files within project with finished game build

---
+++ pong/Assets/SkorSayac.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkorSayac : MonoBehaviour{
+
+public Text Scoreboard;
+public GameObject top;
+    private int Sopa1Skor = 0;
+    private int Sopa2Skor = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 30;
+        top = GameObject.Find ("Top");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+         if (top.transform.position.x >= 24.6f) {
+           Sopa1Skor ++;
+        }
+        if (top.transform.position.x <= -24.6f) {
+           Sopa2Skor ++;
+        }
+                Scoreboard.text = Sopa1Skor.ToString () + "-" + Sopa2Skor.ToString ();
+
+        print (Sopa1Skor + "," + Sopa2Skor);
+
+        
+    }
+}
---
+++ pong/Assets/topkontrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topkontrol : MonoBehaviour
+
+{
+private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {   rb = GetComponent<Rigidbody2D> () ;
+
+        StartCoroutine (Pause());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(this.transform.position.x) >= 25f) {
+           this.transform.position = new Vector3(0f, 0f, 0f);
+            StartCoroutine (Pause());
+        }
+        
+    }
+
+    IEnumerator Pause () {
+
+        int directionX = Random.Range (-1, 2);
+        int directionY = Random.Range (-1, 2);
+
+        if (directionX == 0) {
+            directionX = 1;}
+
+        rb.velocity = new Vector2 (0f,0f) ;
+        yield return new  WaitForSeconds (2) ;
+        rb.velocity = new Vector2 (10f * directionX, 10f * directionY) ;
+    }
+}
